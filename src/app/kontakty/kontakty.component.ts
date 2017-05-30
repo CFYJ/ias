fe3a0edf2ae682869b41ce6352bdbd5498c339d2
Piotr Kamiński <impart@go2.pl>
2017-05-30 20:35:55 +0200

    working on edit window

--- src/app/kontakty/kontakty.component.ts
+++ src/app/kontakty/kontakty.component.ts
@@ -5,6 +5,8 @@ import { jqxInputComponent } from 'jqwidgets-ts/angular_jqxinput';
 import { DataTableModule, SharedModule } from 'primeng/primeng';
 import { Car } from '../car';
 import { jqxGridComponent } from 'jqwidgets-ts/angular_jqxgrid';
+import { jqxWindowComponent } from 'jqwidgets-ts/angular_jqxwindow';
+import { jqxDateTimeInputComponent } from 'jqwidgets-ts/angular_jqxdatetimeinput';
 
 @Component({
   selector: 'app-kontakty',
@@ -25,107 +27,160 @@ export class KontaktyComponent implements AfterViewInit {
     theme: 'metro'
   };
 
-url = 'http://localhost:5000/api/Delegacja';
-    source =
-    {
-        datatype: 'json',
-        datafields: [
-            { name: 'id' },
-            { name: 'idWystawcy' },
-            { name: 'idDelegowanego' },
-            { name: 'nr' },
-            { name: 'czasOd', type: 'date' },
-            { name: 'czasDo', type: 'date' },
-            { name: 'miejscowosc' },
-            { name: 'cel' },
-            { name: 'srodek' },
-            { name: 'dataWystawienia', type: 'date' },
-            { name: 'wystawil' },
-            { name: 'delegowany' },
-            { name: 'wydzial' }
-        ],
-        id: 'id',
-        url: this.url,
-        updaterow: function (rowid: any, rowdata: any, commit: any) {
-            //rowdata.id = rowdata.uid;
-            let url = 'http://localhost:5000/api/Delegacja/' + rowdata.id;
-            var t = JSON.stringify(rowdata);
-            //var t = $.jQuery.parseJSON(rowdata);
-            $.ajax({
-                cache: false,
-                dataType: 'json',
-                contentType: 'application/json',
-                url: url,
-                data: t, //rowdata,
-                //processData: false,
-                type: 'PUT',
-                success: function (data: any, status: any, xhr: any) {
-                    // update command is executed.
-                    commit(true);
-                },
-                error: function (jqXHR: any, textStatus: any, errorThrown: any) {
-                    alert(textStatus + ' - ' + errorThrown);
-                    commit(false);
-                }
-            });
+  url = 'http://localhost:5000/api/Delegacja';
+  source =
+  {
+    datatype: 'json',
+    datafields: [
+      { name: 'id' },
+      { name: 'idWystawcy' },
+      { name: 'idDelegowanego' },
+      { name: 'nr' },
+      { name: 'czasOd', type: 'date' },
+      { name: 'czasDo', type: 'date' },
+      { name: 'miejscowosc' },
+      { name: 'cel' },
+      { name: 'srodek' },
+      { name: 'dataWystawienia', type: 'date' },
+      { name: 'wystawil' },
+      { name: 'delegowany' },
+      { name: 'wydzial' }
+    ],
+    id: 'id',
+    url: this.url,
+    updaterow: function (rowid: any, rowdata: any, commit: any) {
+      let url = 'http://localhost:5000/api/Delegacja/' + rowdata.id;
+      let t = JSON.stringify(rowdata);
+      $.ajax({
+        cache: false,
+        dataType: 'json',
+        contentType: 'application/json',
+        url: url,
+        data: t,
+        type: 'PUT',
+        success: function (data: any, status: any, xhr: any) {
+          commit(true);
+        },
+        error: function (jqXHR: any, textStatus: any, errorThrown: any) {
+          alert(textStatus + ' - ' + errorThrown);
+          commit(false);
         }
-    };
-    dataAdapter = new $.jqx.dataAdapter(this.source, {
-        formatData: function (data: any) {
-            return data;
-        }
-        //loadComplete: function(data: any){alert('data loaded'+data.cel);}
-    });
-    filtergroup = new $.jqx.filter();
-    options: jqwidgets.GridOptions =
-    {
-        localization: { pagergotopagestring: 'Idź do', pagerrangestring: 'z', pagershowrowsstring: 'Liczba wierszy' },
-        autoheight: true,
-        theme: 'metro',
-        width: 1400,
-        pageable: true,
-        pagesize: 20,
-        filterable: true,
-        autoshowfiltericon: true,
-        filtermode: 'excel',
-        showfilterrow: true,
-        editable: false,
-        selectionmode: 'singlerow',
-        //pagerrenderer: pagerenderer,
-        sortable: true,
-        //rendertoolbar: this.rendertoolbar,
-        source: this.dataAdapter,
-        columnsresize: true,
-        columns: [
-            { text: 'Nr', datafield: 'nr', width: 100 },
-            { text: 'Data od', datafield: 'czasOd', width: 100, columntype: 'date', cellsformat: 'yyyy-MM-dd' },
-            { text: 'Data do', datafield: 'czasDo', width: 100, cellsformat: 'yyyy-MM-dd' },
-            { text: 'Miejscowość', datafield: 'miejscowosc', width: 250 },
-            { text: 'Cel', datafield: 'cel', width: 250 },
-            { text: 'Środek', datafield: 'srodek', width: 100 },
-            { text: 'Data wystawienia', datafield: 'dataWystawienia', width: 125, cellsformat: 'yyyy-MM-dd' },
-            { text: 'Wystawił', datafield: 'wystawil', width: 250, hidden: true },
-            { text: 'Delegowany', datafield: 'delegowany', width: 250 },
-            { text: 'Wydział', datafield: 'wydzial', width: 100 },
-            {
-                text: 'Edycja', datafield: 'edycja', width: 50, columntype: 'button',
-                cellsrenderer: function () { return 'Edycja'; }
-            }
-        ],
-        showtoolbar: true,
-        toolbarheight: 50
-    };
+      });
+    }
+  };
+  dataAdapter = new $.jqx.dataAdapter(this.source, {
+    formatData: function (data: any) {
+      return data;
+    }
+  });
+  filtergroup = new $.jqx.filter();
+  options: jqwidgets.GridOptions =
+  {
+    localization: { pagergotopagestring: 'Idź do', pagerrangestring: 'z', pagershowrowsstring: 'Liczba wierszy' },
+    autoheight: true,
+    theme: 'metro',
+    width: '100%',
+    pageable: true,
+    pagesize: 20,
+    filterable: true,
+    autoshowfiltericon: true,
+    filtermode: 'excel',
+    showfilterrow: true,
+    editable: false,
+    selectionmode: 'singlerow',
+    sortable: true,
+    source: this.dataAdapter,
+    columnsresize: true,
+    columns: [
+      { text: 'Nr', datafield: 'nr', width: 100 },
+      { text: 'Data od', datafield: 'czasOd', width: 100, columntype: 'date', cellsformat: 'yyyy-MM-dd' },
+      { text: 'Data do', datafield: 'czasDo', width: 100, cellsformat: 'yyyy-MM-dd' },
+      { text: 'Miejscowość', datafield: 'miejscowosc', width: 250 },
+      { text: 'Cel', datafield: 'cel', width: 250 },
+      { text: 'Środek', datafield: 'srodek' },
+      { text: 'Data wystawienia', datafield: 'dataWystawienia', width: 125, cellsformat: 'yyyy-MM-dd' },
+      { text: 'Wystawił', datafield: 'wystawil', width: 250, hidden: true },
+      { text: 'Delegowany', datafield: 'delegowany', width: 250 },
+      { text: 'Wydział', datafield: 'wydzial', width: 100 },
+      {
+        text: 'Edycja', datafield: 'edycja', width: 50, columntype: 'button',
+        cellsrenderer: function () { return 'Edycja'; }
+      }
+    ]
+  };
 
-@ViewChild('gridReference') myGrid: jqxGridComponent;
+  @ViewChild('gridReference') myGrid: jqxGridComponent;
+  @ViewChild('jqxwindow1') editWindow: jqxWindowComponent;
+  @ViewChild('jqxdelegowany1') jqxdelegowany: jqxInputComponent;
+  @ViewChild('dateInput') myDateInput: jqxDateTimeInputComponent;
 
   constructor(private kontaktyService: KontaktyService) { }
 
-  
   ngAfterViewInit(): void {
+    let dateInputSettings: jqwidgets.DateTimeInputOptions = { width: '300px', height: '25px', theme: 'metro' };
+    let inputSettings: jqwidgets.InputOptions = { width: '300px', height: '25px', theme: 'metro' };
+
+
     let myInput: jqwidgets.jqxInput = jqwidgets.createInstance($('#input'), 'jqxInput', this.ioptions);
     this.kontaktyService.getCarsSmall().then(cars => this.cars = cars);
+
     this.myGrid.createComponent(this.options);
+    this.editWindow.createWidget({
+      width: 500, height: 400, theme: 'metro',
+      resizable: false, isModal: true, autoOpen: false, cancelButton: $('#Cancel'), modalOpacity: 0.5
+    });
+    this.myDateInput.createComponent(dateInputSettings);
+    let t: jqwidgets.jqxInput = jqwidgets.createInstance($('#delegowany'), 'jqxInput', inputSettings);
+
+    //this.jqxdelegowany.createComponent({theme: 'metro'});
+    let buttonOptions: jqwidgets.ButtonOptions = { theme: 'metro' };
+
+    let mySaveButton: jqwidgets.jqxButton = jqwidgets.createInstance($('#Save'), 'jqxButton', buttonOptions);
+    let myCancelButton: jqwidgets.jqxButton = jqwidgets.createInstance($('#Cancel'), 'jqxButton', buttonOptions);
+
+    mySaveButton.addEventHandler('click', (event: any) => {
+      var data = this.myGrid.getrowdata(this.myGrid.getselectedrowindex());
+      //data.miejscowosc = $('#miejscowosc').val();
+
+      var czasOd: Date = new Date($('#czasOd').val());
+      var czasDo: Date = new Date($('#czasDo').val());
+
+      var row = {
+        id: data.id, idWystawcy: data.idWystawcy, idDelegowanego: data.idDelegowanego,
+        nr: data.nr, czasOd: czasOd, czasDo: czasDo,
+        miejscowosc: $('#miejscowosc').val(),
+        cel: $('#cel').val(), srodek: $('#srodek').val(), dataWystawienia: data.dataWystawienia,
+        wystawil: '', delegowany: $('#delegowany').val(), wydzial: data.wydzial
+        //            quantity: parseInt($("#quantity").jqxNumberInput('decimal')),
+        //price: parseFloat($("#price").jqxNumberInput('decimal'))
+      };
+      //var t = JSON.stringify(row);
+      this.myGrid.updaterow(this.myGrid.getrowid(this.myGrid.getselectedrowindex()), row);
+      this.editWindow.close();
+      //alert('saved! ' + $('#miejscowosc').val());
+    });
   };
+
+  editCellclick(event: any): void {
+    if (event.args.datafield === 'edycja') {
+      console.log('cell clicked: ' + event.args.rowindex + ': ' + event.args.datafield);
+      var datarow = event.args.row.bounddata;
+      //$('#delegowany').jqxInput({theme: 'metro', width: 200});
+      //this.delegowany.val(datarow.delegowany);
+      //this.myDateInput.value(datarow.czasDo);
+      $('#id').val(datarow.id);
+      $('#nr').val(datarow.nr);
+      $('#czasOd').val(datarow.czasOd);
+      $('#czasDo').val(datarow.czasDo);
+      $('#miejscowosc').val(datarow.miejscowosc);
+      $('#cel').val(datarow.cel);
+      $('#srodek').val(datarow.srodek);
+      $('#dataWystawienia').val(datarow.dataWystawienia);
+      $('#delegowany').val(datarow.delegowany);
+      this.editWindow.open();
+      //+this.myGrid.getrowdata(this.myGrid.getselectedrowindex()));
+    }
+  }
 
 }
 
