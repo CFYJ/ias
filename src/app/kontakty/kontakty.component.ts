bad79ccfea98723f898da1044179b673810ed160
Piotr Kamiński <impart@go2.pl>
2017-07-10 07:11:08 +0200

    version implemented into production

--- src/app/kontakty/kontakty.component.ts
+++ src/app/kontakty/kontakty.component.ts
@@ -24,8 +24,6 @@ export class KontaktyComponent implements AfterViewInit, OnDestroy {
   subscription: Subscription;
   jednostki: string[];
 
-  url: string = this.sg['SERVICE_URL'] + 'Kontakties/GetKontakty';
-
   source =
   {
     datatype: 'json',
@@ -47,8 +45,7 @@ export class KontaktyComponent implements AfterViewInit, OnDestroy {
       { name: 'wewnetrzny', type: 'string' }
     ],
     id: 'id',
-    url: this.url,
-    // url1: 'ala ma kota',
+    url: this.sg['SERVICE_URL'] + 'Kontakties/GetKontakty',
     updaterow: (rowid: any, rowdata: any, commit: any) => {
       const t = JSON.stringify(rowdata);
       $.ajax({
@@ -146,7 +143,6 @@ export class KontaktyComponent implements AfterViewInit, OnDestroy {
 
   constructor(private kontaktyService: KontaktyService, private authService: AuthenticationService,
     private messageService: MessageService, private sg: SimpleGlobal) {
-    // this.url1 = sg['SERVICE_URL'] + 'Kontakties/PutKontakty/';
   }
 
 
@@ -164,7 +160,6 @@ export class KontaktyComponent implements AfterViewInit, OnDestroy {
       wewnetrzny: this.myWewnetrzny.val(), login: data.login
     };
     // this.myGrid.updaterow(this.myGrid.getrowid(this.myGrid.getselectedrowindex()), row);
-    // let c = this.sg['SERVICE_URL'] + 'Kontakties/PutKontakty/'
     this.myGrid.updaterow(this.myGrid.getrowid(rowindex), row);
     this.editWindow.close();
   }
@@ -207,51 +202,51 @@ export class KontaktyComponent implements AfterViewInit, OnDestroy {
   editCellclick(event: any): void {
     if (event.args.datafield === 'edycja') {
       console.log('cell clicked: ' + event.args.rowindex + ': ' + event.args.datafield);
-      // if (this.authService.loggedIn()) {
-      const datarow = event.args.row.bounddata;
-      // const user = this.authService.getUser();
-      // const userData = this.authService.getUserData();
-      // if (this.authService.checkIfUserHasPermissionToEdit(datarow)) {
-      this.kontaktyService.getJednostki().subscribe(
-        jed => { this.myJednostka.createComponent({ source: jed, width: '300px' }); this.myJednostka.val(datarow.jednostka); });
-      this.kontaktyService.getStanowiska().subscribe(
-        jed => { this.myStanowisko.createComponent({ source: jed, width: '300px' }); this.myStanowisko.val(datarow.stanowisko); });
-      this.kontaktyService.getWydzialy().subscribe(
-        jed => { this.myWydzial.createComponent({ source: jed, width: '300px' }); this.myWydzial.val(datarow.wydzial); });
-      this.kontaktyService.getWydzialyPodlegle().subscribe(
-        jed => {
-          this.myWydzialPodlegly.createComponent({ source: jed, width: '300px' });
-          this.myWydzialPodlegly.val(datarow.wydzial_podlegly);
-        });
-      this.kontaktyService.getPiony().subscribe(
-        jed => {
-          this.myPion.createComponent({ source: jed, width: '300px' });
-          this.myPion.val(datarow.pion);
-        });
-      this.kontaktyService.getMiejscaPracy().subscribe(
-        jed => {
-          this.myMiejscePracy.createComponent({ source: jed, width: '300px' });
-          this.myMiejscePracy.val(datarow.miejsce_pracy);
-        });
+      if (this.authService.loggedIn()) {
+        const datarow = event.args.row.bounddata;
+        // const user = this.authService.getUser();
+        // const userData = this.authService.getUserData();
+        if (this.authService.checkIfUserHasPermissionToEdit(datarow)) {
+          this.kontaktyService.getJednostki().subscribe(
+            jed => { this.myJednostka.createComponent({ source: jed, width: '300px' }); this.myJednostka.val(datarow.jednostka); });
+          this.kontaktyService.getStanowiska().subscribe(
+            jed => { this.myStanowisko.createComponent({ source: jed, width: '300px' }); this.myStanowisko.val(datarow.stanowisko); });
+          this.kontaktyService.getWydzialy().subscribe(
+            jed => { this.myWydzial.createComponent({ source: jed, width: '300px' }); this.myWydzial.val(datarow.wydzial); });
+          this.kontaktyService.getWydzialyPodlegle().subscribe(
+            jed => {
+              this.myWydzialPodlegly.createComponent({ source: jed, width: '300px' });
+              this.myWydzialPodlegly.val(datarow.wydzial_podlegly);
+            });
+          this.kontaktyService.getPiony().subscribe(
+            jed => {
+              this.myPion.createComponent({ source: jed, width: '300px' });
+              this.myPion.val(datarow.pion);
+            });
+          this.kontaktyService.getMiejscaPracy().subscribe(
+            jed => {
+              this.myMiejscePracy.createComponent({ source: jed, width: '300px' });
+              this.myMiejscePracy.val(datarow.miejsce_pracy);
+            });
 
-      this.myImie.val(datarow.imie);
-      this.myNazwisko.val(datarow.nazwisko);
-      this.myPokoj.val(datarow.pokoj);
-      this.myEmail.val(datarow.email);
-      this.myTelefon.val(datarow.telefon);
-      this.myKomorka.val(datarow.komorka);
-      this.myLogin.val(datarow.login);
-      this.myWewnetrzny.val(datarow.wewnetrzny);
+          this.myImie.val(datarow.imie);
+          this.myNazwisko.val(datarow.nazwisko);
+          this.myPokoj.val(datarow.pokoj);
+          this.myEmail.val(datarow.email);
+          this.myTelefon.val(datarow.telefon);
+          this.myKomorka.val(datarow.komorka);
+          this.myLogin.val(datarow.login);
+          this.myWewnetrzny.val(datarow.wewnetrzny);
 
-      this.editWindow.open();
-      //   } else {
-      //     $('#notificationContent').html('Możesz edytować jedynie swoje dane');
-      //     this.msgNotification.open();
-      //   }
-      // } else {
-      //   $('#notificationContent').html('W celu edycji danych należy się zalogować');
-      //   this.msgNotification.open();
-      // }
+          this.editWindow.open();
+        } else {
+          $('#notificationContent').html('Możesz edytować jedynie swoje dane');
+          this.msgNotification.open();
+        }
+      } else {
+        $('#notificationContent').html('W celu edycji danych należy się zalogować');
+        this.msgNotification.open();
+      }
     }
   }
 
