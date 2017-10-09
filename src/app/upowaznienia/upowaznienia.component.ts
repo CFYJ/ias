a6f1122b4446ae0ac11a63f5fa146d636a773b7e
slay3434 <slay3434@o2.pl>
2017-10-09 11:28:55 +0200

    edycja upowaznienia

--- src/app/upowaznienia/upowaznienia.component.ts
+++ src/app/upowaznienia/upowaznienia.component.ts
@@ -6,6 +6,11 @@ import { UpowaznieniaService } from './../upowaznienia.service';
 import { MessageService } from './../message.service';
 import { Subscription } from 'rxjs/Subscription';
 import { AuthenticationService } from './../authentication.service';
+import { jqxWindowComponent } from 'jqwidgets-ts/angular_jqxwindow';
+import { jqxDateTimeInputComponent } from 'jqwidgets-ts/angular_jqxdatetimeinput';
+import { jqxNotificationComponent } from 'jqwidgets-ts/angular_jqxnotification';
+import { jqxButtonComponent } from 'jqwidgets-ts/angular_jqxbuttons';
+import { jqxInputComponent } from 'jqwidgets-ts/angular_jqxinput';
 
 @Component({
   selector: 'app-upowaznienia',
@@ -16,7 +21,7 @@ import { AuthenticationService } from './../authentication.service';
 })
 
 //export class UpowaznieniaComponent implements OnInit {
-  export class UpowaznieniaComponent implements OnInit{
+  export class UpowaznieniaComponent implements OnInit, AfterViewInit{
 
   constructor(private upowaznieniaService: UpowaznieniaService,
     private authService: AuthenticationService,private messageService: MessageService,
@@ -30,8 +35,8 @@ import { AuthenticationService } from './../authentication.service';
   
 
   ngOnInit() {
-    const _self = this;
-    this.myGrid.createComponent(this.options);
+    // const _self = this;
+    // this.myGrid.createComponent(this.options);
   }
 
 
@@ -60,9 +65,31 @@ import { AuthenticationService } from './../authentication.service';
   };
 
   ngAfterViewInit(): void {
-    // const _self = this;
-    // this.myGrid.createComponent(this.options);
+     const _self = this;
+     this.myGrid.createComponent(this.options);
+     const inputSettings: jqwidgets.InputOptions = { width: '300px', height: '25px', theme: 'metro' };
 
+     this.editWindow.createWidget({
+      width: 450, height: 530, theme: 'metro',
+      resizable: false, isModal: true, autoOpen: false, modalOpacity: 0.5
+    });
+
+    const buttonOptions: jqwidgets.ButtonOptions = { theme: 'metro' };
+    this.mySaveButton1.createComponent(buttonOptions);
+    this.myCancelButton1.createComponent(buttonOptions);
+    this.myInsertButton1.createComponent(buttonOptions);
+
+    this.fNazwa.createComponent(inputSettings);
+    this.fNazwa_skrocona.createComponent(inputSettings);
+    this.fWniosek_nadania_upr.createComponent(inputSettings);
+    this.fNadajacy_upr.createComponent(inputSettings);
+    this.fProwadzacy_rejstr_uzyt.createComponent(inputSettings);
+    this.fWniosek_odebrania_upr.createComponent(inputSettings);
+    this.fOdbierajacy_upr.createComponent(inputSettings);
+    this.fOpiekun.createComponent(inputSettings);
+    this.fAdres_email.createComponent(inputSettings);
+    this.fDecyzja.createComponent(inputSettings);
+    this.fUwagi.createComponent(inputSettings);
   }
 
 
@@ -97,6 +124,45 @@ import { AuthenticationService } from './../authentication.service';
       // record: 'Product',
       // id: 'ProductID',
       // url: '../sampledata/products.xml'
+
+      addrow: (rowid: any, rowdata: any, position: any, commit: any) => {
+        const t = JSON.stringify(rowdata);
+        $.ajax({
+          cache: false,
+          dataType: 'json',
+          contentType: 'application/json',
+          url: this.sg['SERVICE_URL'] + 'Upowaznienia/PostUpowaznienia',
+          data: t,
+          type: 'POST',
+          success: function (data: any, status: any, xhr: any) {
+            //alert('Wstawiono nowy rekord - id: ' + data.id);
+            commit(true);
+          },
+          error: function (jqXHR: any, textStatus: any, errorThrown: any) {
+            alert(textStatus + ' - ' + errorThrown);
+            commit(false);
+          }
+        })
+      },
+      updaterow: (rowid: any, rowdata: any, commit: any) => {
+        const t = JSON.stringify(rowdata);
+        $.ajax({
+          cache: false,
+          dataType: 'json',
+          contentType: 'application/json',
+          url: this.sg['SERVICE_URL'] + 'Upowaznienia/PutUpowaznienia/' + rowdata.id,
+          data: t,
+          type: 'PUT',
+          success: function (data: any, status: any, xhr: any) {
+            commit(true);
+          },
+          error: function (jqXHR: any, textStatus: any, errorThrown: any) {
+            alert(textStatus + ' - ' + errorThrown);
+            commit(false);
+          }
+        });
+      }
+
   };
 
   // dataAdapter: any = new $.jqx.dataAdapter(this.source);
@@ -107,6 +173,89 @@ import { AuthenticationService } from './../authentication.service';
   });
 
   @ViewChild('gridReference') myGrid: jqxGridComponent;
+  @ViewChild('jqxwindow1') editWindow: jqxWindowComponent;
+  @ViewChild('nazwa') fNazwa: jqxInputComponent;
+  @ViewChild('nazwa_skrocona') fNazwa_skrocona: jqxInputComponent;
+  @ViewChild('wniosek_nadania_upr') fWniosek_nadania_upr: jqxInputComponent;
+  @ViewChild('nadajacy_upr') fNadajacy_upr: jqxInputComponent;
+  @ViewChild('prowadzacy_rejestr_uzyt') fProwadzacy_rejstr_uzyt: jqxInputComponent;
+  @ViewChild('wniosek_odebrania_upr') fWniosek_odebrania_upr: jqxInputComponent;
+  @ViewChild('odbierajacy_upr') fOdbierajacy_upr: jqxInputComponent;
+  @ViewChild('opiekun') fOpiekun: jqxInputComponent;
+  @ViewChild('adres_email') fAdres_email: jqxInputComponent;
+  @ViewChild('decyzja') fDecyzja: jqxInputComponent;
+  @ViewChild('uwagi') fUwagi: jqxInputComponent;
+
+  @ViewChild('buttonReference') mySaveButton1: jqxButtonComponent;
+  @ViewChild('buttonReference1') myCancelButton1: jqxButtonComponent;
+  @ViewChild('buttonReference2') myInsertButton1: jqxButtonComponent;
+
+
+  buttonSaveClicked(){
+    let data = { id: null };
+    let rowindex: number;
+    if (!this.isInsertOperation) {
+      rowindex = this.myGrid.getselectedcell().rowindex;  
+      data = this.myGrid.getrowdata(rowindex);
+    }
+    const row = {
+      id: data.id, nazwa: this.fNazwa.val(), nazw_skrocona: this.fNazwa_skrocona.val(),
+      wniosek_nadania_upr: this.fWniosek_nadania_upr.val(), nadajacy_upr: this.fNadajacy_upr.val(),
+      prowadzacy_rejestr_uzyt: this.fProwadzacy_rejstr_uzyt.val(), wniosek_odebrania_upr: this.fWniosek_odebrania_upr.val(),
+      odbierajacy_upr: this.fOdbierajacy_upr.val(), opiekun: this.fOpiekun.val(),
+      adres_email: this.fAdres_email.val(), decyzja: this.fDecyzja.val(), uwagi: this.fUwagi.val()
+    };
+    // this.myGrid.updaterow(this.myGrid.getrowid(this.myGrid.getselectedrowindex()), row);
+
+    if (this.isInsertOperation) {
+      row.id = 0;     
+      this.myGrid.addrow(null, row, 'top');
+    } else {
+      this.myGrid.updaterow(this.myGrid.getrowid(rowindex), row);
+    }
+
+    this.isInsertOperation = false;
+    this.editWindow.close();
+
+  }
+
+  buttonCancelClicked() {
+    this.isInsertOperation = false;
+    this.editWindow.close();
+  }
+
+  button2Clicked() {
+    const datarow: any = {
+      nazwa: '', nazwa_skrocona: '', wniosek_nadania_upr: '', nadajacy_upr: '', prowadzacy_rejestr_uzyt: '', wniosek_odebrania_upr: '',
+      odbierajacy_upr: '', opiekun: '', adres_email: '', decyzja: '', uwagi: ''
+    }
+      ;
+    // if (this.initialLoad) { this.loadDropDownValues(); }
+    this.isInsertOperation = true;
+    // this.myLogin.disabled(!this.authService.checkIfUserBelongsToITStaff());
+    this.editWindow.title('Dodawanie');
+    this.editWindow.open();
+  }
+
+  editCellclick(event: any): void {
+    if (event.args.datafield === 'edycja') {
+      console.log('cell clicked: ' + event.args.rowindex + ': ' + event.args.datafield);
+     // if (this.authService.loggedIn()) {
+        const datarow = event.args.row.bounddata;
+        //if (this.authService.checkIfUserHasPermissionToEdit(datarow)) {
+          this.setEditValues(datarow);
+          this.editWindow.title('Edycja');
+          this.editWindow.open();
+        // } else {
+        //   $('#notificationContent').html('Możesz edytować jedynie swoje dane');
+        //   this.msgNotification.open();
+        // }
+      // } else {
+      //   $('#notificationContent').html('W celu edycji danych należy się zalogować');
+      //   this.msgNotification.open();
+      // }
+    }
+  }
 
 
 
@@ -136,7 +285,12 @@ import { AuthenticationService } from './../authentication.service';
   columns: any[] =
   [
 
-
+    {
+      text: '', datafield: 'edycja', width: 50, columntype: 'button', filterable: false,
+      cellsrenderer: function (row, columnfield, value, defaulthtml, columnproperties, rowdata) {
+        return 'Edycja';
+      },
+    },
       { text: 'Nazwa', datafield: 'nazwa', width: 200,  },
       { text: 'Nazwa skrócona',  datafield: 'nazwa_skrocona', width: 100},
       { text: 'Wniosek o nadanie<br> uprawnień', datafield: 'wniosek_nadania_upr', width: 200 },
@@ -150,6 +304,21 @@ import { AuthenticationService } from './../authentication.service';
       { text: 'Uwagi', datafield: 'uwagi', width: 200 },
      
   ];
+
+  setEditValues(datarow: any): any{
+    this.fNazwa.val(datarow.nazwa);
+    this.fNazwa_skrocona.val(datarow.nazw_skrocona);
+    this.fWniosek_nadania_upr.val(datarow.wniosek_nadania_upr);
+    this.fNadajacy_upr.val(datarow.nadajacy_upr);
+    this.fProwadzacy_rejstr_uzyt.val(datarow.prowadzacy_rejestr_uzyt);
+    this.fWniosek_odebrania_upr.val(datarow.wniosek_odebrania_upr);
+    this.fOdbierajacy_upr.val(datarow.odbierajacy_upr);
+    this.fOpiekun.val(datarow.opiekun);
+    this.fAdres_email.val(datarow.adres_email);
+    this.fDecyzja.val(datarow.decyzja);
+    this.fUwagi.val(datarow.uwagi);
+  }
+
 
 
 }
