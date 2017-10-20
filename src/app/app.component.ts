da44738398ec332b55c2a1dbaae611711bedd047
slay3434 <slay3434@o2.pl>
2017-10-20 14:36:48 +0200

    aaa
    aaa
    
    aaa test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,19 +12,31 @@ export class AppComponent {
         const url: string = this.document.location.href;
 
         if (url.startsWith('https')) {
-          sg['HTTPS_SERVICE_URL'] = 'https://127.0.0.1:5000/api/'
-          sg['SERVICE_URL'] = 'https://127.0.0.1:5000/api/'
+          sg['HTTPS_SERVICE_URL'] = 'https://127.0.0.1:5001/api/'
+          sg['SERVICE_URL'] = 'https://127.0.0.1:5001/api/'
         } else if (url.startsWith('http')) {
-          sg['HTTPS_SERVICE_URL'] = 'http://127.0.0.1:5000/api/'
-          sg['SERVICE_URL'] = 'http://127.0.0.1:5000/api/'
+          sg['HTTPS_SERVICE_URL'] = 'http://127.0.0.1:5001/api/'
+          sg['SERVICE_URL'] = 'http://127.0.0.1:5001/api/'
          //sg['SERVICE_URL'] = 'http://localhost:5000/api/'
         }
+
+
+
         // if (url.startsWith('https')) {
-        //   sg['HTTPS_SERVICE_URL'] = 'https://10.10.0.250/api/'
-        //   sg['SERVICE_URL'] = 'https://10.10.0.250/api/'
+        //   sg['HTTPS_SERVICE_URL'] = 'https://10.10.0.250:5001/api/'
+        //   sg['SERVICE_URL'] = 'https://10.10.0.250:5001/api/'
         // } else if (url.startsWith('http')) {
-        //   sg['HTTPS_SERVICE_URL'] = 'http://10.10.0.250/api/'
-        //   sg['SERVICE_URL'] = 'http://10.10.0.250/api/'
+        //   sg['HTTPS_SERVICE_URL'] = 'http://10.10.0.250:5001/api/'
+        //   sg['SERVICE_URL'] = 'http://10.10.0.250:5001/api/'
+        // }
+
+
+        // if (url.startsWith('https')) {
+        //   sg['HTTPS_SERVICE_URL'] = 'https://10.10.0.250:/api/'
+        //   sg['SERVICE_URL'] = 'https://10.10.0.250:8080/api/'
+        // } else if (url.startsWith('http')) {
+        //   sg['HTTPS_SERVICE_URL'] = 'http://10.10.0.250:8080/api/'
+        //   sg['SERVICE_URL'] = 'http://10.10.0.250:8080/api/'
         // }
     }
 }
