988c0bb7cc5298cfd152d862a6c70829914bd055
slay3434 <slay3434@o2.pl>
2018-05-15 15:08:18 +0200

    auth - poprawki w autryzacji, wyeliminowanie błędy typu przy sprawdzaniu ról użytkownika

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -86,8 +86,12 @@ export class AuthenticationService {
       if (token) {
         var role = this.jwtHelper.decodeToken(token)["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"];
 
-        if(role)
-        var result = role.find(function(rola){ return rola.trim().toUpperCase() === dataRow.toUpperCase();});
+        if(role){
+          if(role instanceof Array)
+            var result = role.find(function(rola){ return rola.trim().toUpperCase() === dataRow.toUpperCase();});
+          else
+            var result = role.trim().toUpperCase() === dataRow.toUpperCase()?role.trim().toUpperCase():'';
+        }
         // console.log(result + ' wasl');
         if(result!='' && result!=null)                  
           {  
