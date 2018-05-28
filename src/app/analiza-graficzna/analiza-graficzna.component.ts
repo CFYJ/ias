7b759340860c99f70d4dbb7620939c4942d4601b
slay3434 <slay3434@o2.pl>
2018-05-28 22:46:03 +0200

    gf - prace

--- src/app/analiza-graficzna/analiza-graficzna.component.ts
+++ src/app/analiza-graficzna/analiza-graficzna.component.ts
@@ -1159,6 +1159,7 @@ export class AnalizaGraficznaComponent implements OnInit, AfterViewInit {
 
   //#region file operations
 
+  showFileInfo: boolean = false;
   fileContent: string="";
   loadFile(){
     this.fileWindow.title("Generowanie grafu na podstawie pliku");
